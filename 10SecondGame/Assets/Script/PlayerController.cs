016e398a0b18882fbc9e3de9892c49feef791963
Hanniee Tran <54680570+hanniee227@users.noreply.github.com>
2020-01-15 17:21:33 -0500

    music/timer Added

--- 10SecondGame/Assets/Script/PlayerController.cs
+++ 10SecondGame/Assets/Script/PlayerController.cs
@@ -5,15 +5,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//allow UI Text
+using System.Threading; //for the countdown timer i guess?
+
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text scoreText;
+    public Text winText;
+    public Text countdownText;
+
+
     private Rigidbody2D rb2d;
+    private float timeLeft = 10.0f;
+    private int score;
+    private bool timer;
+    private bool gameOver;
+
+    public AudioSource musicSource;
+    public AudioClip musicClipWin;
+    public AudioClip musicClipLose;
+    public AudioClip musicBackground;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        score = 0;
+        winText.text = "";
+        countdownText.text = "";
+        musicSource.clip = musicBackground;
+        musicSource.Play();
+        timer = true;
+        //gameOver = false;
+        SetScoreText();
     }
 
     // Update is called once per frame
@@ -25,11 +51,37 @@ public class PlayerController : MonoBehaviour
         rb2d.AddForce(movement * speed);
     }
 
+    private void Update()
+    {
+        if (timer == true) //countdown time code
+        {
+            timeLeft -= Time.deltaTime;
+            countdownText.text = (timeLeft).ToString("Time Left: 0");
+            if (score == 5)
+            {
+                timer = false;
+            }
+            else if (timeLeft < 0 && score <5)
+            {
+                timer = false;
+                Destroy(GameObject.FindWithTag("Bunny"));
+                musicSource.Stop();
+                musicSource.loop = true;
+                musicSource.clip = musicClipLose;
+                musicSource.Play();
+                winText.text = "Game Over :P!";
+            }
+            
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag ("Carrot"))
         {
-            other.gameObject.SetActive(false);
+            score = score + 1; //plus 1 score if pick up a carrot
+            other.gameObject.SetActive(false); //disable visual of the carrot
+            SetScoreText();
         }
     }
 
@@ -44,4 +96,21 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+
+    //===Function===
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 5)
+        {
+            winText.text = "You win! Made by Hanniee Tran.";
+            musicSource.Stop();
+            musicSource.loop = true;
+            musicSource.clip = musicClipWin;
+            musicSource.Play();
+        }
+    }
+
+   
 }
---
+++ 10SecondGame/Assets/Script/gameController.cs
@@ -0,0 +1,45 @@
+﻿/*========Hanniee Tran==========*
+ * =========DIG4715C============*
+ * =======10 Second Game========*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // to use UI text
+using System.Threading; //for the countdown timer i guess?
+
+public class gameController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text winText;
+    public Text countdownText;
+    private float timeLeft = 10.0f;
+    private int score;
+    private bool timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownText.text = "";
+        winText.text = "";
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Points: " + score;
+            winText.text = "You win!\nCreated by Hanniee";
+        
+    }
+}
